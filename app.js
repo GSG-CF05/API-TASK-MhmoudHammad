3f183a242c94cd7aa49890ab5eddbdec892d6ff8
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2022-03-12 15:24:50 +0200

    progress bar rhat shows the vote average relates #7

--- app.js
+++ app.js
@@ -10,23 +10,28 @@ fetch('https://api.themoviedb.org/3/discover/movie?api_key=202b320be51d411e0be98
       const linksArr = [];
       const titlesArr = [];
       const overviewArr= [];
-
+      const voteAverage = [];
+     
       data.results.forEach(element => {
         const link = `https://image.tmdb.org/t/p/w300${element.backdrop_path}`;
 
         linksArr.push(link);
         titlesArr.push(element.original_title);
-        overviewArr.push(element.overview)
+        overviewArr.push(element.overview);
+        voteAverage.push(element.vote_average)
       });
-
+      console.log(voteAverage)
       let count = 0;
       bgIMG();
-      descriptionSection()
+      descriptionSection();
+      progressBarFunc(voteAverage[count])
       rightClick.addEventListener('click', () => {
+        console.log(voteAverage[count])
         if ( count < linksArr.length - 1) {
           count++
           bgIMG();
           descriptionSection();
+           progressBarFunc(voteAverage[count])
         }
       });
       
@@ -35,6 +40,7 @@ fetch('https://api.themoviedb.org/3/discover/movie?api_key=202b320be51d411e0be98
           count--;
           bgIMG();
           descriptionSection();
+          progressBarFunc(voteAverage[count])
         }
        
       });
@@ -54,4 +60,25 @@ fetch('https://api.themoviedb.org/3/discover/movie?api_key=202b320be51d411e0be98
 
 })
 .catch(err => console.log('error', err));
+
+function progressBarFunc(progressEndValue) {
+  let progressBar = document.querySelector(".circular-progress");
+  let valueContainer = document.querySelector(".value-container");
+
+  let progressValue = 0;
+  let speed = 20;
+
+  let progress = setInterval(() => {
+    progressValue++;
+    valueContainer.textContent = `${progressValue}%`;
+    progressBar.style.background = `conic-gradient(
+      #FCDB00 ${progressValue * 3.6}deg,
+      #FBFBFB ${progressValue * 3.6}deg
+    )`;
+   if (progressValue == progressEndValue * 10) {
+    clearInterval(progress);
+  }
+}, speed);
+}
+
    
